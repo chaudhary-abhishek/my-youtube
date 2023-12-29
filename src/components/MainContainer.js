d98818c735a62ab795785b0e1637099101faa599
Abhishek Choudhary <abhishekch115@gmail.com>
2023-12-29 20:41:59 +0530

    initial commit

---
+++ src/components/MainContainer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ButttonList from './ButttonList'
+import VideoContainer from './VideoContainer'
+
+const MainContainer = () => {
+  return (
+    <div>
+        <ButttonList />
+        <VideoContainer />
+    </div>
+  )
+}
+
+export default MainContainer
