50ac5b2f3d3ceee1514cd72aec7c779ef5eecf67
Abhishek Choudhary <abhishekch115@gmail.com>
2024-01-02 21:07:34 +0530

    implemeted debouncing in search api

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,25 +6,32 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
   const [searchText, setSearchText] = useState("");
+  const [searchSuggestion, setSearchSuggestion] = useState([]);
   
   const dispatch = useDispatch();
 
   useEffect(()=>{
-     const debounce = setTimeout(()=>{
+     const timer = setTimeout(()=>{
+      //make an api call on every key press
+      //if the difference between two key press is less than 200 ms decline the api call
+      //only make api call when difference is 200ms
       getSerachSuggestion();
-      },2000)
+      },200)
 
       return()=>{
-        clearInterval(debounce);
+        clearTimeout(timer);
       }
+
+      //whenever new component is getting rendered the old one will be destroyed and just before the old componenet got destroyed return function will be called and in this case old timer will be destroyed
     
     
-  },[searchText])
+  },[searchText]);
 
   const getSerachSuggestion = async()=>{
     const data = await fetch(YOUTUBE_SEARCH_SUGGESTION_API+searchText);
-    const json = await data.json;
-    console.log(json[0]);
+    const json = await data.json();
+    //console.log(json[1]);
+    setSearchSuggestion(json[1]);
 
   }
   const toggleMenuHandler = ()=>{
@@ -36,7 +43,8 @@ const Header = () => {
      
   }
   return (
-    <div className="flex justify-between p-4">
+    <div className="">
+    <div className="flex justify-between p-4 shadow-md">
       <div className="flex h-8">
         <img className="mx-4 cursor-pointer"
         onClick={toggleMenuHandler}
@@ -56,14 +64,21 @@ const Header = () => {
       <div className="grid grid-flow-col w-1/2">
         <input className="hidden sm:block w-full px-6 py-2 border border-gray-600 rounded-l-full col-span-11 " type="text" placeholder="Search" value={searchText} onChange={searchInputChangeHandler} />
         <button className="sm:border rounded-r-full col-span-1  border-gray-600">🔍</button>
+      
       </div>
-
+      
       <div className="flex">
         <img className="h-10"
           alt="profile-logo"
           src="https://www.pngall.com/wp-content/uploads/5/User-Profile-PNG-Image.png"
         />
       </div>
+    </div>
+    <div className="hidden sm:block w-2/5 mx-[240px] md:mx-[280px] lg:mx-[360px] xl:mx-[380px] absolute shadow-xl bg-white rounded-lg list-none">
+      {
+        searchSuggestion.map((suggestion)=><li className="cursor-pointer p-1">🔍{" "+suggestion}</li>)
+      }
+    </div>
     </div>
   );
 };
